50dbcadc4d6a12259972fb72a5baf82b7b928cc5
Thiago Floriano <110630909+thiag0floriano@users.noreply.github.com>
2024-01-31 22:36:04 -0300

    novo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", function(req, res){
 });
 
 app.get("/sobre", function(req, res){
-    res.send("Minha pagina sobre");
+    res.sendFile(__dirname + "/html/sobre.htm");
 });
 
 app.get("/blog", function(req, res){
