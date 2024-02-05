84b7617af12fd2828356e6deab7e2cc8961d5a8a
Thiago Floriano <110630909+thiag0floriano@users.noreply.github.com>
2024-02-05 17:47:33 -0300

    sequelize

---
+++ test.js
@@ -0,0 +1,51 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('teste', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+// sequelize.authenticate().then(function(){
+//     console.log("Conectado com sucesso!")
+// }).catch(function(erro){
+//     console.log("Falha ao conectar: "+erro)
+// })
+
+const Postagen = sequelize.define('postagens',{
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+
+})
+// comando para criar a tabela a cima.
+// Postagen.sync({force: true})
+Postagen.create({
+    titulo: "UM TITULO QUALQUER",
+    conteudo: "dfhsfgfgsdfgfuhjtgnfhfargjghjghsfgsfg fgsdf sdhs sdfsdfhsdfh hsfhsdfhhs"
+})
+
+const Usuaroi = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+// Usuaroi.sync({force: true})
+
+Usuaroi.create({
+    nome: "Thiago",
+    sobrenome: "floriano",
+    idade: 34,
+    email: "thiagozmax1@gmail.com"
+})
