1e6c604dbf65e0bf777718dea91ec99442cbdc3d
Thiago Floriano <110630909+thiag0floriano@users.noreply.github.com>
2024-02-07 11:03:18 -0300

    corigido o handlebars

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const express = require("express");
-const app = express();
-
 
 // app.get("/", function(req, res){
 //     res.sendFile(__dirname + "/html/index.html");
@@ -18,23 +15,25 @@ const app = express();
 //     res.send(req.params);
 // });
 
-const handlebars = require('express-handlebars')
-const Sequelize = require('sequelize')
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars");
+const Sequelize = require("sequelize");
 
 //Config
     //Template Engine
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-    app.set('view engine', 'handlebars')
+    app.engine('handlebars', handlebars.create({defaultLayout: 'main'}).engine);
+    app.set('view engine', 'handlebars');
     //Conexão com o banco de dados MySql
     const sequelize = new Sequelize('teste', 'root', '', {
         host: 'localhost',
         dialect: 'mysql'
-    })
+    });
 
 //Rotas
     app.get('/cad', function(req, res){
-        res.send('ROTA DE CADASTRO DE POSTS')
-    })
+        res.send('ROTA DE CADASTRO DE POSTS');
+    });
 
 app.listen(8786, function(){
     console.log("Servidor Rodando na url http://localhost:8786");
